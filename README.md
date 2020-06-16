081745665647b27c661d53732537d6d3b01fba4b
BirajKhanal <biraj@arch.localdomain>
2020-06-16 19:33:20 +0545

    update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 30DayChallenge
 
-## 1st Day - SnakeGame
-## 2nd Day - 2dAnimation
-## 3rd Day - 3dModeling
+### 1st Day - SnakeGame
+### 2nd Day - 2dAnimation
+### 3rd Day - 3dModeling
+### 4th Day - flask_blog
---
+++ flask_blog/flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '6570bc915464e148a8e28bb0642c69a9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_catagory = 'info'
+
+from flaskblog import routes
